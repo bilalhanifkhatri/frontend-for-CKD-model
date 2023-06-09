d48d113c4ca1f724c0bc17ad5d7aceac3f685743
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2023-06-10 00:19:12 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/frontend-for-CKD-model">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
