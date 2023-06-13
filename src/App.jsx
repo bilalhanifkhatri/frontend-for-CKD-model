86bacdaf75dc35f3a47cee0ed681b087cd983602
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2023-06-13 11:44:44 +0500

    Finished Completely ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-
-import Home from "./components/home/Home";
 import Hello from "./Hello";
 
 const App = () => {
   return (
     <>
-      {/* <Home /> */}
       <Hello />
     </>
   );
